fc58785cea880ba8fcd7658c640d7a8975825d3a
willy <willy8742891@gmail.com>
2025-09-13 16:54:12 +0800

    Add ESLint configuration and dependencies for TypeScript and React

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,21 @@
+const js = require('@eslint/js');
+const globals = require('globals');
+const reactHooks = require('eslint-plugin-react-hooks');
+const reactRefresh = require('eslint-plugin-react-refresh');
+const tseslint = require('typescript-eslint');
+const { globalIgnores } = require('eslint/config');
+
+module.exports = [
+  globalIgnores(['dist']),
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  reactHooks.configs['recommended-latest'],
+  reactRefresh.configs.vite,
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+  },
+];
