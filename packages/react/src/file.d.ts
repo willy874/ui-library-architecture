82af089d428be2975af7d63b885f40b34cd8244f
willy <willy8742891@gmail.com>
2025-09-16 14:00:33 +0800

    feat: update package.json and configuration for style handling and improve code structure

--- packages/react/src/file.d.ts
+++ packages/react/src/file.d.ts
@@ -1,17 +1,17 @@
 declare module '*.svg' {
-  const svg: string
-  export default svg
+  const svg: string;
+  export default svg;
 }
 
 declare module '*.png' {
-  const png: string
-  export default png
+  const png: string;
+  export default png;
 }
 
 declare module '*.svg?react' {
-  import React from 'react'
+  import React from 'react';
 
   interface SvgProps extends React.SVGProps<SVGSVGElement> {}
-  const SvgComponent: React.FC<SvgProps>
-  export default SvgComponent
+  const SvgComponent: React.FC<SvgProps>;
+  export default SvgComponent;
 }
