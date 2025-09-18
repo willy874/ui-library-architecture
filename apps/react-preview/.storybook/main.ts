5f9848abaffe5ce64130fb886300ef25228472ae
willy <willy8742891@gmail.com>
2025-09-18 11:50:30 +0800

    feat: Add Button component stories for shape, size, and variant examples
    - Created shape.tsx to demonstrate different button shapes (rounded, square, pill).
    - Developed size.tsx to showcase button sizes (small, medium, large) and icon integration.
    - Implemented variant.tsx to illustrate various button variants (filled, outlined, text, link) and themes (primary, secondary, inverse).
    - Added imports.ts to manage button and icon imports.
    - Updated Configure.mdx with project configuration guidance and resources.
    - Introduced Field component stories with examples for input, select, textarea, and custom input states.
    - Created Header and Page components with corresponding stories for user interaction.
    - Added CSS styles for button, header, and page components to enhance UI presentation.

--- apps/react-preview/.storybook/main.ts
+++ apps/react-preview/.storybook/main.ts
@@ -1,14 +1,39 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import { cwd } from 'node:process';
+import { resolve } from 'node:path';
 
 import svgr from '@ui-library-architecture/rollup-plugin-svgr';
 
+import type { StorybookConfig } from '@storybook/react-vite';
+import { withFilter } from 'vite';
+
+const rootPath = cwd();
+
 const config: StorybookConfig = {
   viteFinal: async (config) => {
-    config.plugins?.push(svgr());
+    if (!config.plugins) {
+      config.plugins = [];
+    }
+    config.plugins!.push(
+      withFilter(
+        svgr({
+          esbuildOptions: { jsx: 'automatic' },
+        }),
+        { load: { id: /\.svg\?react$/ } },
+      ),
+    );
+    if (!config.resolve) {
+      config.resolve = {};
+    }
+    if (!config.resolve?.alias) {
+      config.resolve.alias = {};
+    }
+    Object.assign(config.resolve!.alias, {
+      '@': resolve(rootPath, 'src'),
+    });
     return config;
   },
 
-  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../src/stories/**/*.mdx', '../src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@chromatic-com/storybook',
     '@storybook/addon-docs',
--- apps/react-preview/src/stories/Button/Button.stories.tsx
+++ apps/react-preview/src/stories/Button/Button.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
-import { Button } from './examples/imports';
+import { Button } from './imports';
 
 const meta = {
   title: 'Example/Button',
