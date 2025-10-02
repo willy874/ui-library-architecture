762d2c7a5e6abcc2e81c351de676c1cfa2ca6494
willy <willy8742891@gmail.com>
2025-10-02 09:44:31 +0800

    feat(ResolveDependencies): enhance resolveDependencies function with options for exclusion

--- packages/builder-base/src/resolve-dependencies.ts
+++ packages/builder-base/src/resolve-dependencies.ts
@@ -1,7 +1,27 @@
+import { NODEJS_EXTERNALS } from './node-externals';
+
+interface ResolveDependenciesOptions {
+  exclude?: (string | RegExp)[];
+  excludeNode?: boolean;
+}
+
 export function resolveDependencies(
   dependencies: Record<string, string> = {},
+  options: ResolveDependenciesOptions = {},
 ): (string | RegExp)[] {
-  return Object.keys(dependencies)
+  const { excludeNode = false } = options;
+  const excludes = [...(options.exclude || [])];
+  if (excludeNode) {
+    excludes.push(...NODEJS_EXTERNALS);
+  }
+  const result: (string | RegExp)[] = Object.keys(dependencies)
+    .filter((dep) => {
+      if (!excludes) return true;
+      return !excludes.some((exclusion) => {
+        return typeof exclusion === 'string' ? exclusion === dep : exclusion.test(dep);
+      });
+    })
     .map((dep) => [dep, new RegExp(`^${dep}/.*`)])
     .flat();
+  return result;
 }
