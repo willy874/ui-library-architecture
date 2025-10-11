ef44931facce57adec579162a3481ff3b238322a
willy <willy8742891@gmail.com>
2025-10-11 13:08:35 +0800

    fix(tsconfig): remove baseUrl option and correct path alias for module resolution

--- apps/react-preview/.storybook/main.ts
+++ apps/react-preview/.storybook/main.ts
@@ -4,35 +4,28 @@ import { resolve } from 'node:path';
 import svgr from '@ui-library-architecture/rollup-plugin-svgr';
 
 import type { StorybookConfig } from '@storybook/react-vite';
-import { withFilter } from 'vite';
+import { mergeConfig, withFilter } from 'vite';
 
 const rootPath = cwd();
 
 const config: StorybookConfig = {
   viteFinal: async (config) => {
-    if (!config.plugins) {
-      config.plugins = [];
-    }
-    config.plugins!.push(
-      withFilter(
-        svgr({
-          esbuildOptions: { jsx: 'automatic' },
-        }),
-        { load: { id: /\.svg\?react$/ } },
-      ),
-    );
-    if (!config.resolve) {
-      config.resolve = {};
-    }
-    if (!config.resolve?.alias) {
-      config.resolve.alias = {};
-    }
-    Object.assign(config.resolve!.alias, {
-      '@': resolve(rootPath, 'src'),
+    return mergeConfig(config, {
+      plugins: [
+        withFilter(
+          svgr({
+            esbuildOptions: { jsx: 'automatic' },
+          }),
+          { load: { id: /\.svg\?react$/ } },
+        ),
+      ],
+      resolve: {
+        alias: {
+          '@': resolve(rootPath, 'src'),
+        },
+      },
     });
-    return config;
   },
-
   stories: ['../src/stories/**/*.mdx', '../src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@chromatic-com/storybook',
