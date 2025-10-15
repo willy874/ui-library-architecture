20a839b203d0f305b1c5314a45c35233c3c19a93
willy <willy8742891@gmail.com>
2025-10-15 13:09:45 +0800

    feat(react-icon): initialize package with SVG icons and TypeScript configuration

---
+++ packages/react-icon/vite.config.ts
@@ -0,0 +1,130 @@
+import { resolve, parse } from 'node:path';
+import type { ParsedPath } from 'node:path';
+import { copyFileSync, readFileSync } from 'node:fs';
+import { defineConfig, withFilter } from 'vite';
+import { globbySync } from 'globby';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+import { NODEJS_EXTERNALS } from '@ui-library-architecture/builder-base';
+import svgr from '@ui-library-architecture/rollup-plugin-svgr';
+
+const rootPath = process.cwd();
+
+const pkgPath = resolve(rootPath, 'package.json');
+const pkg = JSON.parse(readFileSync(pkgPath, 'utf-8'));
+
+const getExternal = (
+  packagesNames: Record<string, string> = {},
+  exclude: (string | RegExp)[] = [],
+) => {
+  return Object.keys(packagesNames).reduce(
+    (acc, name) => {
+      if (
+        exclude.some((item) => {
+          if (typeof item === 'string') return item === name;
+          if (item instanceof RegExp) return item.test(name);
+          return false;
+        })
+      ) {
+        return acc;
+      }
+      acc.push(name);
+      acc.push(new RegExp(`^${name}/.*`));
+      return acc;
+    },
+    [] as (string | RegExp)[],
+  );
+};
+
+const external = [
+  ...NODEJS_EXTERNALS,
+  /^node:/,
+  ...getExternal(pkg?.dependencies),
+  ...getExternal(pkg?.peerDependencies),
+];
+
+const renderBanner = (fileName: string) => {
+  const file = parse(fileName);
+  if (/\.(server|action)\.tsx?/.test(file.name)) {
+    return `'use server';`;
+  }
+  if (isSpecialFile(file)) {
+    return '';
+  }
+  return `'use client';`;
+};
+
+const isSpecialFile = (file: ParsedPath) => ['index', 'styles', 'assets'].includes(file.name);
+
+const assetFileNames = (assetInfo: { name?: string }) => {
+  if (assetInfo.name?.endsWith('.css')) {
+    return 'styles.css';
+  }
+  return assetInfo.name || 'assets/[name].[ext]';
+};
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    dts({
+      tsconfigPath: resolve(rootPath, 'tsconfig.app.json'),
+      entryRoot: 'src',
+      staticImport: true,
+      afterBuild: () => {
+        globbySync(['dist/**/*.d.ts', 'dist/**.d.ts']).map((file) => {
+          // support nodejs commonjs modules consumers
+          copyFileSync(file, file.replace(/\.d\.ts$/, '.d.cts'));
+        });
+      },
+    }),
+    withFilter(
+      svgr({
+        esbuildOptions: { jsx: 'automatic' },
+      }),
+      { load: { id: /\.svg\?react$/ } },
+    ),
+    react(),
+  ],
+  build: {
+    target: 'ES2017',
+    minify: false,
+    lib: {
+      entry: globbySync(['src/main.ts', 'src/**/index.ts']),
+      fileName: (format) => (format === 'es' ? 'index.esm.js' : 'index.cjs.js'),
+    },
+    rollupOptions: {
+      logLevel: 'silent',
+      external,
+      output: [
+        {
+          format: 'cjs',
+          preserveModules: true,
+          preserveModulesRoot: 'src',
+          exports: 'named',
+          entryFileNames: '[name].cjs.js',
+          esModule: true,
+          banner: (x) => renderBanner(x.fileName),
+          assetFileNames,
+          inlineDynamicImports: true,
+        },
+        {
+          format: 'es',
+          preserveModules: true,
+          preserveModulesRoot: 'src',
+          exports: 'named',
+          entryFileNames: '[name].esm.js',
+          banner: (x) => renderBanner(x.fileName),
+          assetFileNames,
+          inlineDynamicImports: true,
+        },
+      ],
+    },
+    cssCodeSplit: false,
+  },
+  resolve: {
+    alias: {
+      '@': resolve(rootPath, 'src'),
+      'styled-system': resolve(rootPath, 'src/styled-system'),
+    },
+  },
+});
