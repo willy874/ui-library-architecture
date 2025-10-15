a81dff99b0944ce4136ec47b643182e2d2e20f54
willy <willy8742891@gmail.com>
2025-10-15 13:47:43 +0800

    refactor(react-icon): restructure icon exports and add type definitions for better clarity

--- packages/react-icon/src/index.ts
+++ packages/react-icon/src/index.ts
@@ -1,4 +1,13 @@
-export { default as CloseIcon } from './x.svg?react';
-export { default as Eye } from './eye.svg?react';
-export { default as EyeSlash } from './eye-slash.svg?react';
-export { default as Spinner } from './spinner.svg?react';
+import { default as Close } from './x.svg?react';
+import { default as Eye } from './eye.svg?react';
+import { default as EyeSlash } from './eye-slash.svg?react';
+import { default as Spinner } from './spinner.svg?react';
+
+interface SvgProps extends React.SVGProps<SVGSVGElement> {}
+
+const CloseIcon = Close as React.FC<SvgProps>;
+const EyeIcon = Eye as React.FC<SvgProps>;
+const EyeSlashIcon = EyeSlash as React.FC<SvgProps>;
+const SpinnerIcon = Spinner as React.FC<SvgProps>;
+
+export { CloseIcon, EyeIcon, EyeSlashIcon, SpinnerIcon };
