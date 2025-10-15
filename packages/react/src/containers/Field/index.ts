e6c5c43885e1d8d8e3647235edf73753e123149c
willy <willy8742891@gmail.com>
2025-10-15 13:59:19 +0800

    refactor: remove unused components and configurations from the react package

--- packages/react/src/containers/Field/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Field } from './Field';
-export { default as Input } from './Input';
-export { default as Select } from './Select';
-export { default as Textarea } from './Textarea';
--- packages/react/src/main.ts
+++ packages/react/src/main.ts
@@ -1,4 +1,3 @@
-import './global.css';
 export * from './components/Button';
 export * from './components/Input';
 export * from './components/Select';
@@ -6,25 +5,6 @@ export * from './components/Textarea';
 export * from './components/Field';
 export * from './components/Portal';
 export * from './components/Dialog';
-
-export * from './containers/Dialog';
-export * from './containers/Field';
-export * from './containers/Button';
-
-const UI_TAG = 'data-ui-library-architecture';
-
-export async function setupStyles(getRootNode?: () => Document | ShadowRoot | HTMLElement) {
-  const hasTag = document.querySelector(`style[${UI_TAG}]`);
-  if (hasTag) {
-    return;
-  }
-  const module = await import('./global.css?inline');
-  const style = document.createElement('style');
-  style.textContent = module.default;
-  style.setAttribute(UI_TAG, '');
-  const node = getRootNode ? getRootNode() : document.head;
-  node.appendChild(style);
-}
 
 export { splitProps } from '@/utils/splitProps';
 export { mergeProps } from '@/utils/mergeProps';
